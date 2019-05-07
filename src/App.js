1050c742452cb6a7a351f76b7526ebbe8b70bb80
KevinAdu <kevyugi@gmail.com>
2019-05-07 22:55:32 +0900

    Minor style changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Nengo Converter</h1>
+        <h1>Japanese Year Converter</h1>
         <DateDisplay />
       </div>
     );
