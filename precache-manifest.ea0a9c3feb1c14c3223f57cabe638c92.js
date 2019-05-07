8448fab58872c297c4488dc5375f21c2fab98643
KevinAdu <kevyugi@gmail.com>
2019-05-07 22:45:39 +0900

    Updates

---
+++ precache-manifest.ea0a9c3feb1c14c3223f57cabe638c92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "409f733d44ddd66635a5",
+    "url": "/static/js/main.337f3c7c.chunk.js"
+  },
+  {
+    "revision": "bd7938a10cf549e55a6d",
+    "url": "/static/js/2.c0deefd1.chunk.js"
+  },
+  {
+    "revision": "409f733d44ddd66635a5",
+    "url": "/static/css/main.32e46a09.chunk.css"
+  },
+  {
+    "revision": "30332776daf00c211160ac655d1c0d7c",
+    "url": "/index.html"
+  }
+];
