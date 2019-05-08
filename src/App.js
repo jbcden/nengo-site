01cce6cb7d068da96baa35a782e3f206845bbc69
KevinAdu <kevyugi@gmail.com>
2019-05-08 22:24:41 +0900

    Added hero component and css grid

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './styles/app.scss';
+import Hero from './components/Hero';
 import DateDisplay from './components/DateDisplay';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Nengo</h1>
-        <h3>Converts Gregorian years into traditional Japanese years</h3>
-        <DateDisplay />
+      <div class="container">
+        <header class="header" />
+        <main class="main">
+          <Hero />
+          <DateDisplay />
+        </main>
+        <footer class="footer" />
       </div>
     );
   }
---
+++ src/components/Hero.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Hero extends Component {
+  render() {
+    return (
+      <div class="hero">
+        <h1>Nengo</h1>
+        <h3>Converts Gregorian years into traditional Japanese years</h3>
+      </div>
+    );
+  }
+}
+
+export default Hero;
