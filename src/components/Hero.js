c0cf2e6ce8d8d8d5995430878860f13317b30659
KevinAdu <kevyugi@gmail.com>
2019-05-13 22:42:13 +0900

    Loads the translations on startup based on the cookie

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import { t } from 'ttag';
 
 class Hero extends Component {
   render() {
     return (
       <div class="hero">
-        <h1>Nengo</h1>
+        <h1>{t`Nengo`}</h1>
         <h3>Converts Gregorian years into traditional Japanese years</h3>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { addLocale, useLocale } from 'ttag';
+import * as cookie from './cookie';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+const LOCALE_COOKIE = '__locale';
+
+function getLocale() {
+  return cookie.get(LOCALE_COOKIE) || 'en';
+}
+
+export function saveLocale(locale) {
+  cookie.set(LOCALE_COOKIE, locale);
+}
+
+const locale = getLocale();
+
+if (locale !== 'en') {
+  const translationsObj = require(`./i18n/${locale}.po.json`);
+  addLocale(locale, translationsObj);
+  useLocale(locale);
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
