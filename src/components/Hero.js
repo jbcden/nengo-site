75bcbe23fc50e517d36c4aa6624a45e28055a777
KevinAdu <kevyugi@gmail.com>
2019-05-22 22:45:20 +0900

    Changed Hero into a stateless component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,15 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { t } from 'ttag';
 
-class Hero extends Component {
-  render() {
-    return (
-      <div class="hero">
-        <h1>{t`Nengo`}</h1>
-        <h3>Converts Gregorian years into traditional Japanese years</h3>
-      </div>
-    );
-  }
-}
+const Hero = () => (
+  <div class="hero">
+    <h1>{t`Nengo`}</h1>
+    <h3>Converts Gregorian years into traditional Japanese years</h3>
+  </div>
+);
 
 export default Hero;
