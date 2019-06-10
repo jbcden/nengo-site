b3d845e42182cffd03520014ebf72f7bc787380d
KevinAdu <kevyugi@gmail.com>
2019-06-11 04:41:59 +0900

    Combined the input and display into its own component

--- src/components/DateDisplay.js
+++ src/components/DateDisplay.js
@@ -1,20 +1,42 @@
 import React, { Component } from 'react';
-import DateInput from './DateInput';
+import PropTypes from 'prop-types';
+import { japaneseYear } from 'nengo';
 
 class DateDisplay extends Component {
-  state = {
-    dateOutput: ''
+  static propTypes = {
+    dateOutput: PropTypes.string.isRequired,
+    validate: PropTypes.func,
+    onInputChange: PropTypes.func
   };
 
-  handleInputChange = date => {
-    this.setState({ dateOutput: date });
+  state = {
+    dateOutput: 'Please enter a year'
+  };
+
+  onInputChange = e => {
+    const yearInput = Number.parseInt(e.target.value);
+    const japaneseYearInfo = japaneseYear(yearInput);
+    const error = this.validate(japaneseYearInfo, yearInput);
+    const value =
+      error ||
+      `${japaneseYearInfo.names.english} Year ${yearInput -
+        japaneseYearInfo.startYear +
+        1}`;
+    this.setState({ dateOutput: value });
+  };
+
+  validate = (japaneseYearInfo, yearInput) => {
+    if (!japaneseYearInfo)
+      return 'The year you have entered is not yet supported.';
+    if (!Number.isInteger(yearInput)) return 'Please enter a valid year';
+    return false;
   };
 
   render() {
     return (
       <div class="date-display">
-        <h2>{this.state.dateOutput || 'Please enter a year'}</h2>
-        <DateInput onInputChange={this.handleInputChange} />
+        <h2>{this.state.dateOutput}</h2>
+        <input type="number" onChange={this.onInputChange} />;
       </div>
     );
   }
