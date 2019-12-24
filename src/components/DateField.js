543156805f44814d0f71ed0c6126ce923206f691
KevinAdu <kevyugi@gmail.com>
2019-12-24 13:04:42 +0000

    Added babel

--- src/components/DateField.js
+++ src/components/DateField.js
@@ -6,11 +6,11 @@ class DateField extends Component {
   static propTypes = {
     dateOutput: PropTypes.string.isRequired,
     validate: PropTypes.func,
-    onInputChange: PropTypes.func
+    onInputChange: PropTypes.func,
   };
 
   state = {
-    dateOutput: 'Please enter a year'
+    dateOutput: 'Please enter a year',
   };
 
   onInputChange = e => {
@@ -19,22 +19,19 @@ class DateField extends Component {
     const error = this.validate(japaneseYearInfo, yearInput);
     const value =
       error ||
-      `${japaneseYearInfo.names.english} Year ${yearInput -
-        japaneseYearInfo.startYear +
-        1}`;
+      `${japaneseYearInfo.names.english} Year ${yearInput - japaneseYearInfo.startYear + 1}`;
     this.setState({ dateOutput: value });
   };
 
   validate = (japaneseYearInfo, yearInput) => {
-    if (!japaneseYearInfo)
-      return 'The year you have entered is not yet supported.';
+    if (!japaneseYearInfo) return 'The year you have entered is not yet supported.';
     if (!Number.isInteger(yearInput)) return 'Please enter a valid year';
     return false;
   };
 
   render() {
     return (
-      <div class="date-display">
+      <div className="date-display">
         <h2>{this.state.dateOutput}</h2>
         <input type="number" onChange={this.onInputChange} />;
       </div>
