8143fe813b66e3556a318af8b5e18372b7e6c15b
KevinAdu <kevyugi@gmail.com>
2020-01-27 21:19:22 +0900

    Refactored DateField into a functional component using hooks

--- src/components/DateField.js
+++ src/components/DateField.js
@@ -1,42 +1,31 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { useState } from 'react';
 import { japaneseYear } from 'nengo';
 
-class DateField extends Component {
-  static propTypes = {
-    dateOutput: PropTypes.string.isRequired,
-    validate: PropTypes.func,
-    onInputChange: PropTypes.func,
-  };
+const DateField = () => {
+  const [dateOutput, setDateOutput] = useState('Please enter a year');
 
-  state = {
-    dateOutput: 'Please enter a year',
-  };
-
-  onInputChange = e => {
-    const yearInput = Number.parseInt(e.target.value);
-    const japaneseYearInfo = japaneseYear(yearInput);
-    const error = this.validate(japaneseYearInfo, yearInput);
-    const value =
-      error ||
-      `${japaneseYearInfo.names.english} Year ${yearInput - japaneseYearInfo.startYear + 1}`;
-    this.setState({ dateOutput: value });
-  };
-
-  validate = (japaneseYearInfo, yearInput) => {
+  const validate = (japaneseYearInfo, yearInput) => {
     if (!japaneseYearInfo) return 'The year you have entered is not yet supported.';
     if (!Number.isInteger(yearInput)) return 'Please enter a valid year';
     return false;
   };
 
-  render() {
-    return (
-      <div className="date-display">
-        <h2>{this.state.dateOutput}</h2>
-        <input type="number" onChange={this.onInputChange} />;
-      </div>
-    );
-  }
-}
+  const onInputChange = e => {
+    const yearInput = Number.parseInt(e.target.value);
+    const japaneseYearInfo = japaneseYear(yearInput);
+    const error = validate(japaneseYearInfo, yearInput);
+    const value =
+      error ||
+      `${japaneseYearInfo.names.english} Year ${yearInput - japaneseYearInfo.startYear + 1}`;
+    setDateOutput(value);
+  };
+
+  return (
+    <div className="date-display">
+      <h2>{dateOutput}</h2>
+      <input type="number" onChange={onInputChange} />;
+    </div>
+  );
+};
 
 export default DateField;
